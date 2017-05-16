929103febc4b50bddc97252afbbfd57b0dc9d408
Bruce Collie <bsc28@cam.ac.uk>
2017-05-16 14:51:02 +0100

    Don't build a library - headers only

--- src/fsm/include/fsm/state.h
+++ src/fsm/include/fsm/state.h
@@ -119,4 +119,21 @@ State State::Combined(Container in)
   return Combined(std::begin(in), std::end(in));
 }
 
+inline std::string State::Dot() const
+{
+  std::stringstream out;
+
+  out << "\"" << name << "\"";
+
+  if(accepting) {
+    out << " [shape=doublecircle]";
+  }
+
+  if(initial) {
+    out << ";\"secret\" [style=invis,shape=point];\"secret\"->\"" << name << "\"";
+  }
+
+  return out.str();
+}
+
 #endif
